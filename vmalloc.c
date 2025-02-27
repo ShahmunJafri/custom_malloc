008ae93c9f55aa7d199badf6f5ea3cbcdf3c4d4e
Jafri <szjafri@ieng6-203.ucsd.edu>
2025-02-26 18:41:31 -0800

    this is my commit message for first submission

--- vmalloc.c
+++ vmalloc.c
@@ -1,8 +1,82 @@
 #include "vm.h"
 #include "vmlib.h"
+#include "stdio.h"
+struct block_header* best_fit(struct block_header*, size_t);
+void block_split(struct block_header*, size_t);
+void set_footer(struct block_header*);
 
 void *vmalloc(size_t size)
 {
-    // TODO
-    return NULL;
+	if(size == 0){
+		return NULL;
+	}
+	
+	size_t size_with_header = size + sizeof(struct block_header);
+	
+	size_t size_aligned = ROUND_UP(size_with_header, BLKALIGN);
+	//printf("The value of size is: %zu\n", size);
+	//printf("The value of size w/ header is: %zu\n", size_with_header);	
+	printf("The value of size is: %zu\n", size_aligned); 
+
+	
+	struct block_header* header = best_fit(heapstart, size_aligned);
+	if(header == NULL){
+		return NULL;
+	}
+
+
+	printf("The value of block_header after best fit: %zu\n", (BLKSZ(header)));	
+	
+	header->size_status = header->size_status | VM_BUSY;
+	if((BLKSZ(header)) > size_aligned){
+		//printf("The value of size aligned and blksize: %zu %zu \n", size_aligned, BLKSZ(header));
+		block_split(header, size_aligned);
+	}			
+	
+	return header + 1; 
+
+}
+
+void set_footer(struct block_header* block){
+	struct block_footer* footer = (struct block_footer*)((char*)(block) + BLKSZ(block) - sizeof(struct block_footer));
+	footer->size = BLKSZ(block);
+}
+
+void block_split(struct block_header* header, size_t size){
+	struct block_header* next_block = (struct block_header*)((char*) header + size);
+		
+//	printf("The value of size is: %p %p\n", header, next_block);
+	
+	next_block->size_status = BLKSZ(header) - size;
+	next_block->size_status = next_block->size_status & ~VM_BUSY;
+	next_block->size_status = next_block->size_status | VM_PREVBUSY;
+	
+	int prev_busy = header->size_status & VM_PREVBUSY;
+	header->size_status = size;
+	header->size_status = header->size_status | VM_BUSY;
+
+	if(prev_busy){
+		header->size_status = header->size_status | VM_PREVBUSY;
+	}
+	set_footer(next_block);
+}
+
+struct block_header* best_fit(struct block_header* header, size_t size){
+	struct block_header* current = header;
+	struct block_header* goal = NULL;
+	size_t current_size;
+		
+	while(current->size_status != VM_ENDMARK){
+		current_size = BLKSZ(current);
+		int isallocated = current->size_status & VM_BUSY;
+		
+		if(current_size == size && !isallocated){
+			return current;
+		} else if(current_size > size && (goal == NULL || BLKSZ(goal) > current_size) && !isallocated){
+			goal = current;
+		}
+		current = (struct block_header*)((char*)current + current_size);
+	}
+	
+	return goal;
 }
--- vmfree.c
+++ vmfree.c
@@ -1,6 +1,6 @@
 #include "vm.h"
 #include "vmlib.h"
-
+#include <stdio.h>
 /**
  * The vmfree() function frees the memory space pointed to by ptr,
  * which must have been returned by a previous call to vmmalloc().
@@ -8,7 +8,61 @@
  * undefined behavior occurs.
  * If ptr is NULL, no operation is performed.
  */
+
+void* coalesce(struct block_header**, struct block_header**);
+void set_block_footer(struct block_header*);
+
 void vmfree(void *ptr)
 {
-    // TODO
+	if(ptr == NULL){
+		return;
+	}
+	
+	struct block_header* curr_block_header = (struct block_header*)((char *)(ptr) - sizeof(struct block_header));
+//	size_t size_with_mask = block_ptr->size_status & VM_BLKSZMASK;
+//	printf("The value of size is: %zu\n",size_with_mask);
+
+	if(!(curr_block_header->size_status & VM_BUSY)){
+		return; 
+	}
+	
+       	if(!(curr_block_header->size_status & VM_PREVBUSY)){
+		struct block_footer* prev_block_footer = (struct block_footer*)((char*)(ptr) - sizeof(struct block_footer));
+		size_t prev_size = prev_block_footer->size;
+		struct block_header* prev_block_header = (struct block_header*)((char *)(ptr) - prev_size);
+		ptr = coalesce(&prev_block_header, &curr_block_header);
+	}	
+
+	struct block_header* next_block_header = (struct block_header*) ((char *) ptr + BLKSZ(curr_block_header));
+	
+	if(!(next_block_header->size_status & VM_BUSY)){
+		ptr = coalesce(&curr_block_header, &next_block_header);
+	}
+	curr_block_header = (struct block_header*) ptr;
+	set_block_footer(curr_block_header);
+	curr_block_header->size_status = curr_block_header->size_status & ~VM_BUSY;
+	struct block_header* next_block_after_coalesce = (struct block_header*) ((char*) ptr + (BLKSZ(curr_block_header)));
+	if(next_block_after_coalesce->size_status == VM_ENDMARK){
+		return;
+	}
+	next_block_after_coalesce->size_status = next_block_after_coalesce->size_status & ~VM_PREVBUSY;
 }
+
+void set_block_footer(struct block_header* block){
+	struct block_footer* footer = (struct block_footer*) (((char *)(block)) + BLKSZ(block) - sizeof(struct block_footer));
+	footer->size = BLKSZ(block);
+}
+
+void *  coalesce(struct block_header** prev, struct block_header** curr){
+	struct block_header* new_block = *prev;
+	size_t prev_size_status = new_block->size_status;
+
+	size_t curr_size = BLKSZ(*curr);
+	size_t prev_size = BLKSZ(*prev);
+
+	new_block->size_status = curr_size + prev_size; 
+	new_block->size_status = new_block->size_status | (prev_size_status & VM_PREVBUSY);
+
+	return (void*) new_block;
+}
+
